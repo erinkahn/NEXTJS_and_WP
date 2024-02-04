83d5791df8226d78f03ec12e63783663ac19bdb6
erin <erinnicolekahn@gmail.com>
2024-02-04 15:04:44 -0600

    readme

--- README.md
+++ README.md
@@ -2,6 +2,8 @@ This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next
 
 ## Getting Started
 
+reference site: [https://github.com/JeffreyChix/nextjs-headless-wordpress/tree/main]
+
 First, run the development server:
 
 ```bash
--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      NEXTJS PRACTICE -- source :
-      https://github.com/JeffreyChix/nextjs-headless-wordpress/tree/main
+      NEXTJS PRACTICE
       <Link href="/blog">Blog</Link>
     </main>
   );
