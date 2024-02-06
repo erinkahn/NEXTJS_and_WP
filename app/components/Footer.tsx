ba077a2b0a8a2485ef624d3eeb2c83091e0ee846
Sr. Software Engineer <erinnicolekahn@gmail.com>
2024-02-06 10:47:51 -0600

    created footer and updated root layout.tsx

---
+++ app/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="flex place-content-center text-center py-5 bg-slate-500 text-white">
+      Footer Goes Here
+    </footer>
+  );
+};
+
+export default Footer;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Nav } from "@/app/components/Nav";
+import Footer from "./components/Footer";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body>
         <Nav />
         <main className="relative overflow-x-hidden">{children}</main>
+        <Footer />
       </body>
     </html>
   );
