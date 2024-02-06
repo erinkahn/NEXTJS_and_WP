03022ca61e6fdc1be0259eae9642f239917f5799
Sr. Software Engineer <erinnicolekahn@gmail.com>
2024-02-06 10:31:51 -0600

    private files with _underscore folder

---
+++ app/_private/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PrivatePage = () => {
+  return (
+    <div>
+      You wont see this page in the browser because of the _underscore folder
+      _private
+    </div>
+  );
+};
+
+export default PrivatePage;
