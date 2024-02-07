da2ad3cb993aeb7945701021535c4a4e9e5a0944
Sr. Software Engineer <erinnicolekahn@gmail.com>
2024-02-07 08:02:12 -0600

    added active nav link with usepathname hook

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,29 +1,37 @@
+"use client";
 import { getPages } from "@/app/lib/service";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-export const Nav = async () => {
-  const pages = await getPages();
+export const Nav = () => {
+  // export const Nav = async () => {
+  //   const pages = await getPages();
+
+  const pathname = usePathname();
+  const NavLinks = [
+    { href: "/", title: "Home" },
+    { href: "/blog", title: "Blog" },
+    { href: "/products", title: "Products" },
+    { href: "/login", title: "Login" },
+  ];
 
   return (
     <header className="container mx-auto py-4 px-4">
       <div className="navbar w-full md:w-3/5 mx-auto border-b-2">
         <nav className="flex items-center justify-center pb-4 text-base md:text-xl">
-          <Link href="/" className="px-4">
-            Home
-          </Link>
-          <Link href="/blog" className="px-4">
-            Blog
-          </Link>
-          <Link href="/products" className="px-4">
-            Products
-          </Link>
-          <Link href="/products/reviews" className="px-4">
-            Reviews
-          </Link>
-          <Link href="/login" className="px-4">
-            Login
-          </Link>
-
+          {NavLinks.map((link) => (
+            <Link
+              key={link.title}
+              href={link.href}
+              className={
+                pathname === link.href
+                  ? `font-bold text-blue-600 px-4`
+                  : `text-black px-4`
+              }
+            >
+              {link.title}
+            </Link>
+          ))}
           {/* 
           // from wordpress
           <ul className="ml-5 flex gap-5">
